5bee4699a9f592ff4e985635d25ad5486dbc5274
Tanuj Jain <tanuj.jain@somaiya.edu>
2020-11-30 14:09:36 +0530

    Final version 1

--- Bars.py
+++ Bars.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 
 
@@ -13,19 +12,3 @@ class Bars:
 
     def draw(self):
         pygame.draw.rect(self.surface, self.color, (self.x, self.y - self.HEIGHT, self.BW, self.HEIGHT))
-=======
-import pygame
-
-
-class Bars:
-    def __init__(self, surface, x,y, height, bw, color):
-        self.surface = surface
-        self.x = x
-        self.y = y
-        self.HEIGHT = height
-        self.BW = bw
-        self.color = color
-
-    def draw(self):
-        pygame.draw.rect(self.surface, self.color, (self.x, self.y - self.HEIGHT, self.BW, self.HEIGHT))
->>>>>>> c1ed4a1235ddc8c2b9c710d75a8b811af5079aa2
